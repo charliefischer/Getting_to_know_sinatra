2054dbea11d7e46b430d195e70410b68a95b980c
Hamish Arro <Hamisharro@icloud.com>
2021-01-18 16:58:23 +0000

    complete Returning HTML

--- app.rb
+++ app.rb
@@ -16,3 +16,9 @@ end
 get '/hidden' do
   "hidden page"
 end
+
+get '/cat' do
+  "<div style='border: 3px dashed red'>
+  <img src='https://product-image.juniqe-production.juniqe.com/media/catalog/product/seo-cache/x800/18/22/18-22-301P__CENTER/Sneaky-Cat-Laura-Graves-Canvas-Print.jpg'}
+  </div>"
+end
