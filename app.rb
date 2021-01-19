c50a56b9ffcf08ce4c2b58618c90e2596e1a7690
Glykeria Stravodimou <strglykeria@gmail.com>
2021-01-19 15:27:05 +0000

    Params

--- app.rb
+++ app.rb
@@ -5,19 +5,13 @@ get '/' do
   "Hello World"
 end
 
-get '/secret' do
-  "secret page"
-end
-
-get '/s' do
-  "s page"
-end
-
-get '/hidden' do
-  "hidden page"
-end
-
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
