f59713e62e874a5f84b59da1d6445bfbf6434cb1
Charlie Fischer <charlie.a.fischer@gmail.com>
2021-01-22 16:30:40 +0000

    refactoring global variables

--- Battle/app.rb
+++ Battle/app.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 require './lib/player.rb'
+require './lib/game.rb'
 
 class Battle < Sinatra::Base
   enable :sessions
@@ -9,22 +10,20 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    $player_1 = Player.new(params[:player_1_name])
-    $player_2 = Player.new(params[:player_2_name])
+    player_1 = Player.new(params[:player_1_name])
+    player_2 = Player.new(params[:player_2_name])
+    $game = Game.new(player_1, player_2)
     redirect '/play'
   end
 
   get '/play' do
-    @player_1_name = $player_1.name
-    @player_2_name = $player_2.name
+    @game = $game
     erb(:play)
   end
 
   get '/attack' do
-    @player_1_name = $player_1.name
-    @player_2_name = $player_2.name
-    Game.new.attack($player_2)
-    @player_2_health = $player_2.hit_points
+    @game = $game
+    $game.attack($game.player_2)
     erb(:attacks)
   end
 
--- Battle/spec/game_spec.rb
+++ Battle/spec/game_spec.rb
@@ -1,14 +1,29 @@
 require 'game'
 
 describe Game do
-  subject(:oscar) { double('Oscar') }
-  subject(:glykeria) { double('Glykeria') }
+  subject(:game) { described_class.new(oscar, glykeria)}
+  subject(:oscar) { double :player }
+  subject(:glykeria) { double :player }
+
+  describe '#player_1' do
+    it 'retrieves the first player' do
+      expect(game.player_1).to eq oscar
+    end
+  end
+
+  describe '#player_2' do
+    it 'retrieves the second player' do
+      expect(game.player_2).to eq glykeria
+    end
+  end
+
 
     describe '#attack' do
       it 'damages the player' do
       expect(oscar).to receive(:receive_damage)
-      Game.new.attack(oscar)
+      game.attack(oscar)
     end
- end
+  end
+
 
 end
